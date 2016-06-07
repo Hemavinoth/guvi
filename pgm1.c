4c632d06c8778d22edd5b9d425559854caa109d1
SUJIN KUMAR A <sujin.kumar3@gmail.com>
2016-06-07 11:58:03 +0800

    Create pgm1.c

---
+++ pgm1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	if(t>0)
+	printf("Positive\n");
+	else if(t<0)
+	printf("Negative\n");
+	else
+	printf("Zero\n");
+	return 0;
+}
